343552d6fbf9994c7f6676227a7d5a5507caece3
Matthew Cwalina <matthewcwalina@gmail.com>
2022-02-16 16:14:47 +0100

    Lifting up state

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button className="square" onClick={()=> this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={()=> this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
@@ -23,13 +23,23 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
-    }
-  };
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares });
+  }
 
   renderSquare(i) {
-    return
-      <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
@@ -57,7 +67,6 @@ class Board extends React.Component {
     );
   }
 }
-
 class Game extends React.Component {
   render() {
     return (
